de08c988d5f5f82559cdd692d6cdf2fab16ea098
Nomthandazo Magwaza <1608789@students.wits.ac.za>
2022-05-23 18:07:56 +0200

    Added Unit Tests

--- __test__/wordle.test.js
+++ __test__/wordle.test.js
@@ -3,19 +3,40 @@
  */
 
 
+//const { test } = require('picomatch');
 const {Wordle} = require('../src/wordle')
 
 test('testing constructor', () => {
     const wordle = new Wordle();
     expect(wordle.newWord).toBe('')
+    //expect(wordle.guessNumber).toBe(1)
 })
 
 test("testing appendLetter function", () => {
     const wordle = new Wordle();
-    letter = 'a';
+    letter = 'a'
     wordle.appendLetter(letter);
     expect(wordle.newWord).toBe('a');
 })
 
+test("Testing enter goes to next line", () => {
+    const wordle = new Wordle();
+    wordle.newWord = "SUPER"
+    wordle.enterWord();
+    expect(wordle.guessNumber).toBe(2);
+})
 
+test("Testing enter does not go to next line when word length is 3", () => {
+    const wordle = new Wordle();
+    wordle.newWord = "SUPE"
+    wordle.enterWord();
+    expect(wordle.guessNumber).toBe(1);
+})
+
+test("Testing enter does not go to next line when word length is 0", () => {
+    const wordle = new Wordle();
+    wordle.newWord = ""
+    wordle.enterWord();
+    expect(wordle.guessNumber).toBe(1);
+})
 
