c5225795a3c440cfac9060b4be68b6c71c61209b
JoeSwaggott <2169705@students.wits.ac.za>
2022-05-23 13:40:55 +0200

    Added storing and minor display of word

--- src/wordle.js
+++ src/wordle.js
@@ -1,6 +1,29 @@
+// Creating a Wordle class
+class Wordle {
+    constructor() {
+        this.newWord = ''
+    }
+
+    appendLetter(letter) {
+        this.newWord = this.newWord + letter
+    }
+
+    updateGrid() {
+        
+        let row = document.querySelector('#row1')
+        let cell = row.querySelector('#col1')
+        cell.innerText = this.newWord[this.newWord.length-1]
+        console.log(this.newWord)
+    }
+}
+
+const wordle = new Wordle()
+
+
 const letterButtons = document.querySelectorAll('button')
 letterButtons.forEach(button => {
     button.addEventListener('click', () => {
-        console.log(button.innerText)
+        wordle.appendLetter(button.innerText);
+        wordle.updateGrid();
     })
 })
