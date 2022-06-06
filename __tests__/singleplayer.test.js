99471de8f69f850045252f838918347393e98f08
Hraklis Papageorgiou <2137492@students.wits.ac.za>
2022-06-06 17:24:39 +0200

    Fixed testing affected by colours

--- __tests__/singleplayer.test.js
+++ __tests__/singleplayer.test.js
@@ -3,11 +3,122 @@
  */
 // const { test } = require('picomatch');
 
-jest.mock('../views/singleplayer.ejs')
+
+
+//'use strict';
+
+jest.mock('../views/singlePlayer.ejs')
 document.body.innerHTML = `
 <div class='wordVal' data-test-value='<%= JSON.stringify('words') %>'></div>
-  `
+<div id="GuessGrid" class="Guess Grid mt-5">
+      <div id="row1" class="row text-center justify-content-center">
+        <div id="col1" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;" >
+          .
+        </div>
+        <div id="col2" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;">
+          .
+        </div>
+        <div id="col3" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;">
+          .
+        </div>
+        <div id="col4" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;">
+          .
+        </div>
+        <div id="col5" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;">
+          .
+        </div>
+      </div>
+      <div id="row2" class="row text-center justify-content-center">
+        <div id="col1" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;;" >
+          .
+        </div>
+        <div id="col2" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;">
+          .
+        </div>
+        <div id="col3" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;">
+          .
+        </div>
+        <div id="col4" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col5" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+      </div>
+      <div id="row3" class="row text-center justify-content-center">
+        <div id="col1" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;" >
+          .
+        </div>
+        <div id="col2" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col3" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col4" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col5" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+      </div>
+      <div id="row4" class="row text-center justify-content-center">
+        <div id="col1" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;" >
+          .
+        </div>
+        <div id="col2" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col3" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col4" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col5" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+      </div>
+      <div id="row5" class="row text-center justify-content-center">
+        <div id="col1" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;" >
+          .
+        </div>
+        <div id="col2" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col3" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col4" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col5" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+      </div>
+      <div id="row6" class="row text-center justify-content-center">
+        <div id="col1" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff;" >
+          .
+        </div>
+        <div id="col2" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col3" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col4" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+        <div id="col5" class="col-1" style = "border:2px solid rgb(88, 86, 86); background-color: #ffffff">
+          .
+        </div>
+      </div>
+    </div>
+`
+require('../public/singleplayer.js')
+const $ = require('jquery');
 const { Wordle } = require('../public/singleplayer.js')
+
 
 test('testing constructor', () => {
   const wordle = new Wordle()
@@ -17,9 +128,9 @@ test('testing constructor', () => {
 
 test('testing appendLetter function', () => {
   const wordle = new Wordle()
-  letter = 'a'
+  letter = 'A'
   wordle.appendLetter(letter)
-  expect(wordle.newWord).toBe('a')
+  expect(wordle.newWord).toBe('A')
 })
 
 test('Testing enter goes to next line', () => {
@@ -72,3 +183,33 @@ test('Testing the the gameResult function will set win to false when the new wor
   wordle.gameResult()
   expect(wordle.win).toBe(false)
 })
+
+test('Update grid can update a single letter cell on the wordle board', () => {
+  const wordle = new Wordle()
+  wordle.newWord = 'H'
+  wordle.updateGrid()
+  const row = document.querySelector('#row1')
+  const cell = row.querySelector('#col1')
+
+  expect(cell.innerText).toBe('H')
+})
+
+test('Update grid can update an entire word on the wordle board', () => {
+  const wordle = new Wordle()
+  word = 'HELLO'
+  wordle.newWord = word
+  wordle.updateGrid()
+
+  rowID = '#row1'
+
+  for (let i = 1; i <= 5; i++){
+    colID = '#col' + i
+    const row = document.querySelector(rowID)
+    const cell = row.querySelector(colID)
+    expect(cell.innerText).toBe(word.charAt(i-1))
+
+  }
+})
+
+
+
