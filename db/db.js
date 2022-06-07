82a38ce1f2d8f1910e0064b795469d2ee3cab61d
JoeSwaggott <2169705@students.wits.ac.za>
2022-06-07 16:54:30 +0200

    fixing bugs

--- db/db.js
+++ db/db.js
@@ -4,9 +4,10 @@ const config = {
   server: 'elen4010-group08-db.database.windows.net',
   database: 'WordleWithFriends',
 
-
-  user: process.env.AzureDBAdmin,
-  password: process.env.AzureDBPassword,
+  user: 'group08admin',
+  password: 'Wordle2Nite',
+  // user: process.env.AzureDBAdmin,
+  // password: process.env.AzureDBPassword,
   port: 1433,
   options: {
     encrypt: true,
